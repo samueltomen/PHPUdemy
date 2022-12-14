0b90d70601161f8549f6d4231bc93706f679bab4
Dytsaw <tomennana.samuel@outlook.fr>
2022-12-14 16:43:42 +0100

    marche pas

--- test1/index.php
+++ test1/index.php
@@ -1,19 +1,20 @@
 <?php
 // Testons si le fichier a bien été envoyé et s'il n'y a pas d'erreur
-if (isset($_FILES['screenshot']) && $_FILES['screenshot']['error'] == 0) {
+if (isset($_FILES['image']) && $_FILES['image']['error'] == 0) {
     // Testons si le fichier n'est pas trop gros
-    if ($_FILES['screenshot']['size'] <= 1000000000) {
+    if ($_FILES['image']['size'] <= 1000000000) {
         // Testons si l'extension est autorisée
-        $fileInfo = pathinfo($_FILES['screenshot']['name']);
+        $fileInfo = pathinfo($_FILES['image']['name']);
         $extension = $fileInfo['extension'];
         $allowedExtensions = ['jpg', 'jpeg', 'gif', 'png'];
         if (in_array($extension, $allowedExtensions)) {
             // On peut valider le fichier et le stocker définitivement
-            move_uploaded_file($_FILES['screenshot']['tmp_name'], 'uploads/' . basename($_FILES['screenshot']['name']));
+            move_uploaded_file($_FILES['screenshot']['tmp_name'], 'uploads/' . basename($_FILES['image']['name']));
             echo "L'envoi a bien été effectué !";
         }
     }
 }
+
 ?>
 
 <!doctype html>
@@ -37,28 +38,13 @@ if (isset($_FILES['screenshot']) && $_FILES['screenshot']['error'] == 0) {
     <main>
         <h2 class="bg-danger p-2 text-center font-size-lg display-2 text-uppercase">Photoshoot</h2>
         <div class="container">
-            <!-- FRMULAIRE -->
-            <article class="col-md-8 mt-5">
-                <h2 class="font-weight-bold text-center">Hébergez une image</h2>
-                <?php
-                if (isset($error) && ($error == 0)) {
-                    echo '<img src="' . $address . '"' . '"width=50px" />';
-                }
-                ?>
-                <form method="post" action="index.php" enctype="multipart/form-data">
-                    <p class="m-5">
-                        <input type="file" required name="screenshot">
-                        <br>
-                        <button class="mt-2" type="submit">Heberger</button>
-                    </p>
-                </form>
-            </article>
+            <!-- F0RMULAIRE -->
             <form action="index.php" method="POST" enctype="multipart/form-data">
                 <!-- Ajout des champs email et message -->
-                [...]
+
                 <!-- Ajout champ d'upload ! -->
-                <div class="mb-3">
-                    <label for="screenshot" class="form-label">Votre capture d'écran</label>
+                <div class="mb-3 mt-5">
+                    <label for="screenshot" class="form-label border border-2 border-dark p-1">Votre image</label>
                     <input type="file" class="form-control" id="screenshot" name="screenshot" />
                 </div>
                 <!-- Fin ajout du champ -->
